e40171a7a5e8407ba5bd45b10de5f8258b08354a
Kebtarum <mura_aktau@mail.ru>
2020-08-01 23:40:31 +0300

    Updates

---
+++ precache-manifest.3bc2a50e39b35de394f7ffce5fa52909.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003ae493a1ee970192a26fd9c9124a93",
+    "url": "/demo-SN/index.html"
+  },
+  {
+    "revision": "71e8e7668fc41989be18",
+    "url": "/demo-SN/static/css/3.abc906c3.chunk.css"
+  },
+  {
+    "revision": "a1f0523d4aaff6af460b",
+    "url": "/demo-SN/static/css/4.e12a6579.chunk.css"
+  },
+  {
+    "revision": "a1d087486ef6b8b33228",
+    "url": "/demo-SN/static/css/main.65a3358c.chunk.css"
+  },
+  {
+    "revision": "198da70466f286ed65d3",
+    "url": "/demo-SN/static/js/2.2e9deec2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-SN/static/js/2.2e9deec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e8e7668fc41989be18",
+    "url": "/demo-SN/static/js/3.3acba25a.chunk.js"
+  },
+  {
+    "revision": "a1f0523d4aaff6af460b",
+    "url": "/demo-SN/static/js/4.ae9081b6.chunk.js"
+  },
+  {
+    "revision": "a1d087486ef6b8b33228",
+    "url": "/demo-SN/static/js/main.00041727.chunk.js"
+  },
+  {
+    "revision": "d4def29e86d5e96ad2ef",
+    "url": "/demo-SN/static/js/runtime-main.33c01e62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-SN/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "221b2f28b16175b4ae120beb211a3652",
+    "url": "/demo-SN/static/media/userPhoto.221b2f28.jpg"
+  }
+]);
